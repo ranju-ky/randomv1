711520b7c9cd3884e216d67b12bcd0b9d6b828b0
MaGa6969 <jymbo.02@gmail.com>
2021-04-30 09:22:21 +0200

    30-4-1

--- main.js
+++ main.js
@@ -2,9 +2,9 @@ const boton = document.getElementById("botton");
 
 let name = ["ana", "marta", "inga", "ranju"];
 
-function random(name){
-   return Math.floor(Math.random() * (name.length));
+function random(){
+   console.log(name[Math.floor(Math.random() * (name.length))]);
 }
-console.log(name);
+
 
 boton.addEventListener("click", random);
