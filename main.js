881e2be346bb48c68e11766c51af0ee5a59a610c
anagarcialainez <accgl@hotmail.com>
2021-05-03 15:20:09 +0200

    pruebaIF/SPLICE

--- main.js
+++ main.js
@@ -4,8 +4,24 @@ let names = ["Alisa", "Amanda", "Ana G.", "Ana L.", "Angie", "Clau", "Daniela R.
    "Pepi", "María Laura", "Marta", "Nicole", "Olga", "Yeraldin", "Ranju", "Rosangely", "Sònia", "Taiza", "Yasmin"];
 
 function showName() {
-   document.getElementById("box").innerHTML = names[Math.floor(Math.random() * (names.length))];
+   let randomName = Math.floor(Math.random() * names.length);
+   console.log("let showme" + randomName);
+   for( let i = 0; i < names.length; i++) {
+     // if ( names[i] === randomName);
+         names.splice(randomName, 1);
+ }
 }
+
+   /*  document.getElementById("box").innerHTML = names[Math.floor(Math.random() * (names.length))];
+
+
+        {
+
+           arr.splice(i, 1);
+           names.splice(-1, 1 );
+     console.log(names);
+  }*/
+
 
 
 boton.addEventListener("click", showName);
