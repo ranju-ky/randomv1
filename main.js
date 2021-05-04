f6c1b636cce542875ae03032a1230c6a113b4593
IngaD89 <inga.demetrashvili.1@iliauni.edu.ge>
2021-05-04 10:56:00 +0200

    funciona splice?!

--- main.js
+++ main.js
@@ -2,27 +2,18 @@ const boton = document.getElementById("botton");
 
 let names = ["Alisa", "Amanda", "Ana G.", "Ana L.", "Angie", "Clau", "Daniela R.", "Daniela T.", "Eliana", "Flo", "Gaby", "Leo", "Inga",
    "Pepi", "María Laura", "Marta", "Nicole", "Olga", "Yeraldin", "Ranju", "Rosangely", "Sònia", "Taiza", "Yasmin"];
+let randomName = names[Math.floor(Math.random()*names.length)];
+console.log(randomName)
+names.splice(randomName, 1);
+//for( let i = 0; i <= names.length; i++) {
+        //if (names[i] === randomName)}
 
 function showName() {
-   let randomName = Math.floor(Math.random() * names.length);
-   console.log("let showme" + randomName);
-   for( let i = 0; i < names.length; i++) {
-     // if ( names[i] === randomName);
-         names.splice(randomName, 1);
- }
+
+    document.getElementById("box").innerHTML = names[Math.floor(Math.random() * (names.length))];
+
+
 }
-
-   /*  document.getElementById("box").innerHTML = names[Math.floor(Math.random() * (names.length))];
-
-
-        {
-
-           arr.splice(i, 1);
-           names.splice(-1, 1 );
-     console.log(names);
-  }*/
-
-
 
 boton.addEventListener("click", showName);
 
