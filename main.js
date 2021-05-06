99b939c5d9e0c876b06823933381496606a06b18
MaGa6969 <jymbo.02@gmail.com>
2021-05-06 11:52:56 +0200

    haciendo pruebas sin resultado para el gif

--- main.js
+++ main.js
@@ -3,33 +3,28 @@ const boton = document.getElementById("botton");
 let names = ["Alisa", "Amanda", "Ana G.", "Ana L.", "Angie", "Clau", "Daniela R.", "Daniela T.", "Eliana", "Flo", "Gaby", "Leo", "Inga",
    "Pepi", "María Laura", "Marta", "Nicole", "Olga", "Yeraldin", "Ranju", "Rosangely", "Sònia", "Taiza", "Yasmin"];
 
-
-
 function showName() {
     let randomNumber = Math.floor(Math.random() * names.length);
-
     document.getElementById("box").innerHTML = names[randomNumber]
-
-  if (names[randomNumber] === undefined) {
+    if (names[randomNumber] === undefined) {
         return document.getElementById('box').innerHTML = 'Please restart';
     }
-
     names.splice(randomNumber, 1);
-
-    document.body.style.backgroundImage = "url('images/cortina.gif')";
-
+    setTimeout(function(){
+        document.body.innerHTML = url("images/cortina.gif");
+    },6000);
 }
-
+/*function timeOut(){
+    setTimeout(function(){
+        document.body.innerHTML = url("images/cortina.gif");
+    },0);
+    }*/
 boton.addEventListener("click", showName);
-
 const reload = document.getElementById("reinicio");
-
 reload.addEventListener('click', _ => {location.reload();});
 
 
-/*if (names > [23]) {
-    console.log('please click restart button');
-}*/
+
 
 
 
