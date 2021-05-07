a1a90a757060e7e3a3a59cb3f877ccc17965814c
Ranju- <ranjuprj98@gmail.com>
2021-05-07 10:38:51 +0200

    cambio de cortina

--- main.js
+++ main.js
@@ -3,7 +3,12 @@ const boton = document.getElementById("botton");
 let names = ["Alisa", "Amanda", "Ana G.", "Ana L.", "Angie", "Clau", "Daniela R.", "Daniela T.", "Eliana", "Flo", "Gaby", "Leo", "Inga",
    "Pepi", "María Laura", "Marta", "Nicole", "Olga", "Yeraldin", "Ranju", "Rosangely", "Sònia", "Taiza", "Yasmin"];
 
+function animateCortina() {
+    document.body.style.backgroundImage= `url("images/cortina.gif")`;
+}
+
 function showName() {
+    animateCortina()
     let randomNumber = Math.floor(Math.random() * names.length);
     document.getElementById("box").innerHTML = names[randomNumber]
     if (names[randomNumber] === undefined) {
