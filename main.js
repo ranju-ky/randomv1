cae4070a0cc8e2df5a573081f9643aa067ab04f6
anagarcialainez <accgl@hotmail.com>
2021-05-07 11:09:20 +0200

    cortina animada

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
 const boton = document.getElementById("botton");
 
 let names = ["Alisa", "Amanda", "Ana G.", "Ana L.", "Angie", "Clau", "Daniela R.", "Daniela T.", "Eliana", "Flo", "Gaby", "Leo", "Inga",
-   "Pepi", "María Laura", "Marta", "Nicole", "Olga", "Yeraldin", "Ranju", "Rosangely", "Sònia", "Taiza", "Yasmin"];
+    "Pepi", "María Laura", "Marta", "Nicole", "Olga", "Yeraldin", "Ranju", "Rosangely", "Sònia", "Taiza", "Yasmin"];
+
+function animateCortina() {
+    document.body.style.backgroundImage= `url("images/cortina.gif")`;
+}
 
 function showName() {
+    animateCortina()
     let randomNumber = Math.floor(Math.random() * names.length);
     document.getElementById("box").innerHTML = names[randomNumber]
     if (names[randomNumber] === undefined) {
@@ -15,9 +20,6 @@ function showName() {
 boton.addEventListener("click", showName);
 const reload = document.getElementById("reinicio");
 reload.addEventListener('click', _ => {location.reload();});
-
-
-
 
 
 
